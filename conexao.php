dcc68a993af35dfb18c7cfc591d75e6130e7341c
christian-777 <christian.c.g.f777@hotmail.com>
2021-12-29 02:09:08 -0300

    index, script de mask junto ao cadastro, banco de dados e cadastro de entidades - 29.12.2021

--- conexao.php
+++ conexao.php
@@ -1,9 +1,9 @@
 <?php
 
-	$host ="localhost";//"us-cdbr-east-05.cleardb.net:3306";
-	$bd = "agendamoura";//"heroku_5d2b1cab6cee9be";
-	$usuario = "root";//"bece118490a044";
-	$senha = "usbw";//"e25429ea";
+	$host ="us-cdbr-east-05.cleardb.net:3306";
+	$bd = "heroku_5d2b1cab6cee9be";
+	$usuario = "bece118490a044";
+	$senha = "e25429ea";
 
 	$con = mysqli_connect($host,$usuario,$senha,$bd) or die("Erro ao abrir a conexão com o banco de dados.");
 
